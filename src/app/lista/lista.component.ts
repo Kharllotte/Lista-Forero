955afd80ad8d2e2bef3a8540176da5c04226da93
Liliana <kharlotte@protonmail.com>
2023-04-21 23:27:11 -0500

    creación de lista ngFor

---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit{
+  public nombre : string | undefined;
+  public personas= ["Liliana", "Daniela"];
+
+  ngOnInit(): void {
+    
+  }
+}
